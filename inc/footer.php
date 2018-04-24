735ea7a9615d7a425d17746f9acfe3813f0455de
kristansmith <getkristan@gmail.com>
2018-04-24 18:12:25 +0200

    Added node

--- inc/footer.php
+++ inc/footer.php
@@ -9,3 +9,6 @@ if (!defined('__CONFIG__')) {
                 crossorigin="anonymous"></script>
         <script src="js/uikit.min.js"></script>
         <script src="js/uikit-icons.min.js"></script>
+
+
+<script src="/assets/js/main.js"></script>
